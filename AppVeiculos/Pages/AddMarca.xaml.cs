77f3d969ec4de7c6132330b554383d8db4daede1
ibqrz <isabelqa77@gmail.com>
2025-03-22 18:39:03 -0300

    att no cs

--- AppVeiculos/Pages/AddMarca.xaml.cs
+++ AppVeiculos/Pages/AddMarca.xaml.cs
@@ -1,9 +1,31 @@
+using Microsoft.Maui.Controls;
+
 namespace AppVeiculos;
 
 public partial class AddMarca : ContentPage
 {
-	public AddMarca()
-	{
-		InitializeComponent();
-	}
+    public AddMarca()
+    {
+        InitializeComponent();
+        btnAddMarca.Clicked += btnAddMarcaOnClicked;
+    }
+
+    private async void btnAddMarcaOnClicked(object? sender, System.EventArgs e)
+    {
+        string marca = etrMarca.Text;
+        string obsMarca = edtOBSMarca.Text;
+
+        // salvar no banco de dados 
+
+        if (string.IsNullOrWhiteSpace(marca))
+        {
+            DisplayAlert("ERRO", "O campo 'Marca' precisa ser preeenchido!", "Ok");
+            return;
+        }
+        
+        await DisplayAlert("Marca Adicionada", $"Marca: {marca}\nObservações: {obsMarca}", "OK");
+            
+        etrMarca.Text = string.Empty;
+        edtOBSMarca.Text = string.Empty;
+    }
 }
--- AppVeiculos/Pages/AddVeiculo.xaml.cs
+++ AppVeiculos/Pages/AddVeiculo.xaml.cs
@@ -1,3 +1,6 @@
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
 namespace AppVeiculos;
 
 public partial class AddVeiculo : ContentPage
@@ -6,4 +9,23 @@ public partial class AddVeiculo : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void btnAddVeiculoOnClicked(object? sender, EventArgs e)
+    {
+        string veiculo = etrVeiculo.Text;
+        // int ano = etrAnoFab; 
+        // string obsVeiculo = edtObsVeiculos.Text;
+
+        if (string.IsNullOrWhiteSpace(veiculo))
+        {
+            DisplayAlert("ERRO", "Os campos 'camposEx' precisam estar preenchidos!", "Ok");
+            return;
+        }
+
+        await DisplayAlert("Veículo Adicionado", $"Veícluo: {veiculo}", "OK");
+
+        etrVeiculo.Text = string.Empty;
+
+    }
+
 }
