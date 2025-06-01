3254e1d4d64d1fb90c52a8f767e8e14f4e43f268
ibqrz <isabelqa77@gmail.com>
2025-06-01 13:50:55 -0300

    veiculos

--- AppVeiculos/Pages/Adicionar/AddVeiculo.xaml.cs
+++ AppVeiculos/Pages/Adicionar/AddVeiculo.xaml.cs
@@ -1,36 +1,109 @@
-using System.Collections.ObjectModel;
-using System.Windows.Input;
+using AppVeiculos.Helpers;
+using AppVeiculos.Models;
+using Microsoft.Maui.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AppVeiculos;
 
 public partial class AddVeiculo : ContentPage
 {
-	public AddVeiculo()
-	{
-		InitializeComponent();
-	}
+    private List<Marcas>? _marcas;
+    private List<AppVeiculos.Models.Modelo>? _modelos;
 
-    private async void btnAddVeiculoOnClicked(object? sender, EventArgs e)
+    public AddVeiculo()
     {
-        string veiculo = etrVeiculo.Text;
-        // int ano = etrAnoFab.Text; 
-        // string obsVeiculo = edtObsVeiculo.Text;
+        InitializeComponent();
+        CarregarMar();
+        CarregarMod();
+    }
 
-        // salvar no banco de dados
+    // Este construtor parece ser um resquício ou está sendo usado de forma incorreta.
+    // Se a intenção é editar um veículo, o BindingContext deve ser definido
+    // antes de navegar para a página EdtVeiculo.
+    // Mantenho-o comentado ou removido para evitar confusão no fluxo de "adição".
+    /*
+    public AddVeiculo(IEnumerable<Veiculo> vei, object veiNome)
+    {
+        InitializeComponent();
+        btnAddVeiculo.Clicked += btnAddVeiculoOnClicked;
+        // Lógica para carregar os dados do veículo para edição, se for o caso
+        // Esta página é AddVeiculo, então este construtor não é apropriado para ela.
+    }
+    */
 
-        if (string.IsNullOrWhiteSpace(veiculo))
+    private async void CarregarMar()
+    {
+        _marcas = await App.Db.GetAllMarca();
+        if (_marcas != null && _marcas.Any())
         {
-            await DisplayAlert("ERRO", "Verifique se os campos estão preenchidos!", "Ok");
+            pkMar.ItemsSource = _marcas.Select(m => m.marNome).ToList();
+        }
+    }
+
+    private async void CarregarMod()
+    {
+        _modelos = await App.Db.GetAllModelo();
+        if (_modelos != null && _modelos.Any())
+        {
+            pkMod.ItemsSource = _modelos.Select(m => m.modNome).ToList();
+        }
+    }
+
+    private async void btnAddVeiculoOnClicked(object? sender, System.EventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(etrVeiculo.Text) ||
+            string.IsNullOrWhiteSpace(etrAnoFab.Text) ||
+            pkMar.SelectedIndex == -1 || 
+            pkMod.SelectedIndex == -1)   
+        {
+            await DisplayAlert("ERRO", "Todos os campos (Nome, Ano, Marca, Modelo) precisam ser preenchidos!", "Ok");
+            return; 
+        }
+
+        if (!int.TryParse(etrAnoFab.Text, out int anoFab))
+        {
+            await DisplayAlert("ERRO", "Ano de Fabricação deve ser um número válido.", "Ok");
             return;
         }
 
-        await DisplayAlert("Veículo Adicionado", $"Veículo: {veiculo}", "OK");
-        await Shell.Current.GoToAsync("//Veiculos");
+        int? selectedMarcaId = null;
+        if (pkMar.SelectedIndex != -1 && _marcas != null)
+        {
+            selectedMarcaId = _marcas[pkMar.SelectedIndex].marId;
+        }
 
-        etrVeiculo.Text = string.Empty;
-        // etrAnoFab.Text = string.Empty;
-        // edtObsVeiculo.Text = string.Empty;
+        int? selectedModeloId = null;
+        if (pkMod.SelectedIndex != -1 && _modelos != null)
+        {
+            selectedModeloId = _modelos[pkMod.SelectedIndex].modId;
+        }
 
+        if (!selectedMarcaId.HasValue || !selectedModeloId.HasValue)
+        {
+            await DisplayAlert("ERRO", "Não foi possível obter o ID da Marca ou Modelo selecionado.", "Ok");
+            return;
+        }
+
+        Veiculo vei = new Veiculo
+        {
+            veiNome = etrVeiculo.Text,
+            veiAnoFab = anoFab,
+            veiObs = edtObsVeiculo.Text,
+            codMar = selectedMarcaId.Value, 
+            codMod = selectedModeloId.Value 
+        };
+
+        try
+        {
+            await App.Db.InsertVeiculo(vei);
+            await DisplayAlert("Sucesso!", "Veículo adicionado.", "OK");
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", $"Ocorreu um erro ao adicionar o veículo: {ex.Message}", "OK");
+        }
     }
-
 }
--- AppVeiculos/Pages/Veiculos.xaml.cs
+++ AppVeiculos/Pages/Veiculos.xaml.cs
@@ -1,28 +1,240 @@
-using System.Collections.ObjectModel;
-using System.Windows.Input;
+using AppVeiculos.Helpers;
+using AppVeiculos.Models;
 using Microsoft.Maui.Controls;
+using SQLite;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AppVeiculos;
 
 public partial class Veiculos : ContentPage
 {
-	public Veiculos()
-	{
-		InitializeComponent();
-	}
+    private List<Marcas> _marcas;
+    private List<AppVeiculos.Models.Modelo> _modelos;
 
-	private async void btnAddVeiculoOnClicked(object sender, EventArgs e)
-	{
-		await Navigation.PushAsync(new AddVeiculo());
-	}
+    public Veiculos()
+    {
+        InitializeComponent();
+    }
 
-	private async void imgEditOnClicked(object sender, EventArgs e)
-	{
-		await Navigation.PushAsync(new EdtVeiculo());
-	}
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await CarregarVeiculoNoContainer(string.Empty);
+    }
 
-	private async void imgDeleteOnClicked(object sender, EventArgs e)
-	{
-		await DisplayAlert("Excluído", "O veículo foi deletado!", "Ok");
-	}
+    private async void btnAddVeiculoOnClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AddVeiculo()); 
+    }
+    private async Task CarregarVeiculoNoContainer(string searchTerm = "")
+    {
+
+        if (cardsContainer == null)
+        {
+            Console.WriteLine("ERRO: cardsContainer é nulo. XAML pode não ter sido inicializado.");
+            return;
+        }
+
+        cardsContainer.Children.Clear();
+
+        List<Veiculo> veiculos;
+
+        if (string.IsNullOrWhiteSpace(searchTerm))
+        {
+            veiculos = await App.Db.GetAllVeiculo();
+        }
+        else
+        {
+            veiculos = await App.Db.SearchVeiculo(searchTerm);
+        }
+
+        if (veiculos != null && veiculos.Any())
+        {
+            foreach (var vei in veiculos)
+            {
+                var frame = new Frame
+                {
+                    BackgroundColor = Color.FromArgb("#404040"),
+                    Padding = 10,
+                    WidthRequest = 350,
+                    CornerRadius = 10,
+                    Margin = new Thickness(10),
+                    HasShadow = true,
+                    Content = new Grid
+                    {
+                        ColumnDefinitions =
+                        {
+                            new ColumnDefinition { Width = GridLength.Auto },
+                            new ColumnDefinition { Width = GridLength.Star }
+                        },
+                        RowDefinitions =
+                        {
+                            new RowDefinition { Height = GridLength.Auto },
+                            new RowDefinition { Height = GridLength.Auto },
+                            new RowDefinition { Height = GridLength.Auto },
+                            new RowDefinition { Height = GridLength.Auto },
+                            new RowDefinition { Height = GridLength.Auto },
+                            new RowDefinition { Height = GridLength.Auto },
+                            new RowDefinition { Height = GridLength.Auto }
+                        }
+                    }
+                };
+
+                var gridContent = (Grid)frame.Content;
+
+                var image = new Image
+                {
+                    Source = "vehicles.png",
+                    Aspect = Aspect.AspectFit,
+                    HeightRequest = 100,
+                    WidthRequest = 100,
+                    Margin = new Thickness(0, 0, 10, 0),
+                    VerticalOptions = LayoutOptions.Start,
+                    HorizontalOptions = LayoutOptions.Start,
+                };
+                gridContent.Children.Add(image);
+                Grid.SetColumn(image, 0);
+                Grid.SetRow(image, 0);
+                Grid.SetRowSpan(image, 2);
+
+
+                var labelId = new Label
+                { 
+                    Text = $"ID: {vei.veiId}",
+                    TextColor = Colors.White,
+                    FontSize = 14,
+                    FontAttributes = FontAttributes.Bold,
+                    VerticalOptions = LayoutOptions.Center,
+                };
+                Grid.SetColumn(labelId, 1);
+                Grid.SetRow(labelId, 0);
+                gridContent.Children.Add(labelId);
+
+
+                var labelVeiculo = new Label
+                {
+                    Text = $"Veículo: {vei.veiNome}",
+                    TextColor = Colors.White,
+                    FontAttributes = FontAttributes.Bold,
+                    FontSize = 14,
+                    VerticalOptions = LayoutOptions.Center,
+                };
+                Grid.SetColumn(labelVeiculo, 1);
+                Grid.SetRow(labelVeiculo, 1);
+                gridContent.Children.Add(labelVeiculo);
+
+
+                var labelAnoFab = new Label
+                {
+                    Text = $"Ano: {vei.veiAnoFab}",
+                    TextColor = Colors.White,
+                    FontAttributes = FontAttributes.Bold,
+                    FontSize = 14,
+                    VerticalOptions = LayoutOptions.Center,
+                };
+                Grid.SetColumn(labelAnoFab, 1);
+                Grid.SetRow(labelAnoFab, 2);
+                gridContent.Children.Add(labelAnoFab);
+
+
+                var marcaAssociada = _marcas?.FirstOrDefault(m => m.marId == vei.codMar);
+                var modeloAssociado = _modelos?.FirstOrDefault(m => m.modId == vei.codMod);
+
+                var labelCodMar = new Label
+                {
+                    Text = $"Marca:  {(marcaAssociada != null ? marcaAssociada.marNome : "N/A")}",
+                    TextColor = Colors.White,
+                    FontAttributes = FontAttributes.Bold,
+                    FontSize = 14,
+                    VerticalOptions = LayoutOptions.Center,
+                };
+                Grid.SetColumn(labelCodMar, 1);
+                Grid.SetRow(labelCodMar, 3);
+                gridContent.Children.Add(labelCodMar);
+
+
+                var labelCodMod = new Label
+                {
+                    Text = $"Modelo: {(modeloAssociado != null ? modeloAssociado.modNome : "N/A")}",
+                    TextColor = Colors.White,
+                    FontAttributes = FontAttributes.Bold,
+                    FontSize = 14,
+                    VerticalOptions = LayoutOptions.Center,
+                };
+                Grid.SetColumn(labelCodMod, 1);
+                Grid.SetRow(labelCodMod, 4);
+                gridContent.Children.Add(labelCodMod);
+
+
+                var labelObs = new Label
+                {
+                    Text = $"Obs: {vei.veiObs}",
+                    TextColor = Colors.White,
+                    FontAttributes = FontAttributes.Bold,
+                    FontSize = 14,
+                    LineBreakMode = LineBreakMode.WordWrap,
+                    VerticalOptions = LayoutOptions.Center,
+                    HorizontalOptions = LayoutOptions.Fill,
+                };
+                Grid.SetColumn(labelObs, 1);
+                Grid.SetRow(labelObs, 5);
+                gridContent.Children.Add(labelObs);
+
+
+                var buttonsLayout = new StackLayout
+                {
+                    Orientation = StackOrientation.Horizontal,
+                    HorizontalOptions = LayoutOptions.End,
+                    Spacing = 10,
+                    Margin = new Thickness(0, 10, 0, 0),
+                    Children =
+                    {
+                        new ImageButton
+                        {
+                            Source = "edit.png",
+                            HeightRequest = 30,
+                            WidthRequest = 30,
+                            Command = new Command(async () =>
+                            {
+                                var editVeiculoPage = new EdtVeiculo();
+                                editVeiculoPage.BindingContext = vei;
+                                await Navigation.PushAsync(editVeiculoPage);
+                            })
+                        },
+                        new ImageButton
+                        {
+                            Source = "cancel.png",
+                            HeightRequest = 30,
+                            WidthRequest = 30,
+                            Command = new Command(async () =>
+                            {
+                                bool confirm = await DisplayAlert("Confirmação", $"Deseja realmente excluir o veículo '{vei.veiNome}'?", "Sim", "Não");
+                                if (confirm)
+                                {
+                                    await App.Db.DeleteVeiculo(vei.veiId);
+                                    await DisplayAlert("Excluída", "O veículo foi deletado!", "Ok");
+                                    await CarregarVeiculoNoContainer(SearchBarVeiculo.Text);
+                                }
+                            })
+                        }
+                    }
+                };
+                Grid.SetRow(buttonsLayout, 6);
+                Grid.SetColumn(buttonsLayout, 0);
+                Grid.SetColumnSpan(buttonsLayout, 2);
+                gridContent.Children.Add(buttonsLayout);
+
+                cardsContainer.Children.Add(frame);
+            }
+        }
+    }
+
+    private async void txtSearchTextChanged(object sender, TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+        await CarregarVeiculoNoContainer(q);
+    }
 }
